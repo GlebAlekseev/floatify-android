b5399579bae6294bab3cf26a016127e36caf28c9
GlebAlekseev <glebalekseevjk@gmail.com>
2023-02-20 21:01:54 +0300

    added shared prefs temporary storage

---
+++ app/src/main/java/com/glebalekseevjk/floatify_android/data/preferences/SharedPreferencesTemporaryStorage.kt
@@ -0,0 +1,27 @@
+package com.glebalekseevjk.floatify_android.data.preferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import javax.inject.Inject
+
+open class SharedPreferencesTemporaryStorage @Inject constructor(private val context: Context) {
+    private val temporaryPref: SharedPreferences =
+        context.getSharedPreferences(PREF_PACKAGE_NAME, Context.MODE_PRIVATE)
+
+    fun getYandexTranslateSessionId(): String? {
+        return temporaryPref.getString(PREF_KEY_YANDEX_TRANSLATE_SESSION_ID, null)
+    }
+
+    fun setYandexTranslateSessionId(sessionId: String) {
+        temporaryPref.edit().putString(PREF_KEY_YANDEX_TRANSLATE_SESSION_ID, sessionId).apply()
+    }
+
+    fun clear() {
+        temporaryPref.edit().remove(PREF_KEY_YANDEX_TRANSLATE_SESSION_ID).apply()
+    }
+
+    companion object {
+        private const val PREF_PACKAGE_NAME = "com.glebalekseevjk.floatify"
+        private const val PREF_KEY_YANDEX_TRANSLATE_SESSION_ID = "yandex_translate_session_id"
+    }
+}
